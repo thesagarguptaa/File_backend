d886a0cd449283c16f4a550ff8636e91ded5b045
Sagar Gupta <sg4270173@gmail.com>
2023-09-05 12:33:36 -0700

    data base url

--- Config/database.js
+++ Config/database.js
@@ -3,10 +3,13 @@ require("dotenv").config();
 
 const dbConnect = () => {
   mongoose
-    .connect(process.env.DATABASE_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
+    .connect(
+      "mongodb+srv://sg4270173:aZ0NxXMqgUWHLJ96@cluster0.3y2asss.mongodb.net/CloudDataBase",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    )
 
     .then(() => console.log("DB connection is successful"))
 
